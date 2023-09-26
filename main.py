11aba7f08c82555fef06e8a05292a2c31e47711c
Joshua Canales <joshua.canales0712@gmail.com>
2023-09-26 21:45:05 +0100

    Dataset changes

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import numpy as np
-import pandas as pd 
 import matplotlib.pyplot as plt
-import numpy as np
-import PIL
 import tensorflow as tf
 
 from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 import pathlib
-from IPython.display import display
+
 
 class IndianActorClassification:
 
@@ -19,31 +15,113 @@ class IndianActorClassification:
         self.image_height = height
         self.batch_size = batch_size
 
-        # Import data
-        dataset_dir =  pathlib.Path("dataset/Bollywood Actor Images")
-        label_dir = pathlib.Path("dataset/List of Actors.txt")
+        # Datset and label paths
+        self.dataset_dir =  pathlib.Path("dataset/Bollywood Actor Images")
+        self.label_dir = pathlib.Path("dataset/List of Actors.txt")
+        
+        
 
-        train_ds = keras.utils.image_dataset_from_directory(
-            dataset_dir,
+        # Augumentation function (Helps with overfitting)
+        data_augmentation = keras.Sequential([
+            layers.RandomFlip("horizontal",
+            input_shape=(self.image_height,self.image_width,3)),
+            layers.RandomRotation(0.1),
+            layers.RandomZoom(0.1),
+        ])
+
+        self.train_ds = keras.utils.image_dataset_from_directory(
+            self.dataset_dir,
             validation_split=0.2,
             subset='training',
             seed=7,
             image_size = (self.image_height,self.image_width),
-            batch_size=batch_size
+            batch_size=self.batch_size
         )
 
-        val_ds = keras.utils.image_dataset_from_directory(
-            dataset_dir,
+        
+        self.val_ds = keras.utils.image_dataset_from_directory(
+            self.dataset_dir,
             validation_split=0.2,
             subset='validation',
             seed=7,
             image_size = (self.image_height,self.image_width),
-            batch_size=batch_size
+            batch_size=self.batch_size
         )
 
-    def summary(self):
-        pass
+        class_names = self.train_ds.class_names
+        num_classes = len(class_names)
 
+        self.model = Sequential([
+            data_augmentation,
+            layers.Rescaling(1./255),
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(128, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(256, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Dropout(0.2),
+            layers.Flatten(),
+            layers.Dense(516, activation='relu'),
+            layers.Dense(num_classes, name="outputs")
+        ])
+
+        AUTOTUNE = tf.data.AUTOTUNE
+
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)  
+
+    def summary(self):
+        self.model.summary()
 
     def train(self): 
-        pass       
+        self.model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+        results = self.model.fit(
+            self.train_ds,
+            validation_data=self.val_ds,
+            epochs=self.epochs
+        )
+
+        return results
+
+    def plotLoss(self, results):
+        acc = results.history['accuracy']
+        val_acc = results.history['val_accuracy']
+
+        loss = results.history['loss']
+        val_loss = results.history['val_loss']
+
+        epochs_range = range(self.epochs)
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
+
+def main():
+    classModel = IndianActorClassification()
+    print(classModel.summary())
+    results = classModel.train()
+    classModel.plotLoss(results)
+
+
+    
+
+if __name__ == "__main__":
+    main()
